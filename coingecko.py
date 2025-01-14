26eeb2c9ec183e3ec5e3ed14be21b46ce21248f8
JacobJanuary <jacob.smartfox@gmail.com>
2025-01-14 22:17:04 +0300

    8.3 coingecko app

--- coingecko.py
+++ coingecko.py
@@ -19,8 +19,7 @@ mysql = MySQL(app)
 def index():
     try:
         cur = mysql.connection.cursor()
-        # Вызываем функцию cg_GetFilteredCoins с параметрами по умолчанию:
-        # p_vol_min: 10000, p_growth6h: 100, p_growth1h: 100, p_price_change_max: 10, p_price_change_min: 0, p_market_cap_rank: NULL
+        # Используем CALL вместо SELECT для хранимой процедуры
         query = "CALL cg_GetFilteredCoins(10000, 100, 100, 10, 0, NULL)"
         cur.execute(query)
         rows = cur.fetchall()
