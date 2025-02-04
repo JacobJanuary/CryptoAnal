bab06178b26d52962957a23ca2cd923bff7eea5e
elcrypto <jacob.smartfox@gmail.com>
2025-02-04 22:03:29 +0700

    13.2

--- upgrade categoriy_relations.py
+++ upgrade categoriy_relations.py
@@ -117,7 +117,7 @@ def get_coin_ids_for_update():
         conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor()
         # Выбираем все записи, где description_en  NULL иои пустое (после обрезки пробелов)
-        query = "SELECT `id` FROM `coin_gesco_coins"
+        query = "SELECT `id` FROM `coin_gesco_coins`"
         cursor.execute(query)
         rows = cursor.fetchall()
         coin_ids = [row[0] for row in rows]
